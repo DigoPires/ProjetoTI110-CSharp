1b1976181933b36d174834698aa9cc5acdd04887
DigoPires <rodrigo_pires2806@outlook.com>
2023-10-26 17:05:22 -0300

    Commit

---
+++ EmpresaSA/ProjetoLojaABC/frmWebCam.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace ProjetoLojaABC
+{
+    partial class frmWebCam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pctCapturarImagem = new System.Windows.Forms.PictureBox();
+            this.btnCapturar = new System.Windows.Forms.Button();
+            this.lblDispositivo = new System.Windows.Forms.Label();
+            this.cbbDispositivo = new System.Windows.Forms.ComboBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.sfdSalvarImagem = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctCapturarImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pctCapturarImagem
+            // 
+            this.pctCapturarImagem.Location = new System.Drawing.Point(38, 32);
+            this.pctCapturarImagem.Name = "pctCapturarImagem";
+            this.pctCapturarImagem.Size = new System.Drawing.Size(319, 289);
+            this.pctCapturarImagem.TabIndex = 0;
+            this.pctCapturarImagem.TabStop = false;
+            // 
+            // btnCapturar
+            // 
+            this.btnCapturar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.btnCapturar.Location = new System.Drawing.Point(413, 43);
+            this.btnCapturar.Name = "btnCapturar";
+            this.btnCapturar.Size = new System.Drawing.Size(201, 61);
+            this.btnCapturar.TabIndex = 1;
+            this.btnCapturar.Text = "Capturar";
+            this.btnCapturar.UseVisualStyleBackColor = true;
+            this.btnCapturar.Click += new System.EventHandler(this.btnCapturar_Click);
+            // 
+            // lblDispositivo
+            // 
+            this.lblDispositivo.AutoSize = true;
+            this.lblDispositivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblDispositivo.Location = new System.Drawing.Point(35, 353);
+            this.lblDispositivo.Name = "lblDispositivo";
+            this.lblDispositivo.Size = new System.Drawing.Size(76, 17);
+            this.lblDispositivo.TabIndex = 4;
+            this.lblDispositivo.Text = "Dispositivo";
+            // 
+            // cbbDispositivo
+            // 
+            this.cbbDispositivo.FormattingEnabled = true;
+            this.cbbDispositivo.Location = new System.Drawing.Point(38, 377);
+            this.cbbDispositivo.Name = "cbbDispositivo";
+            this.cbbDispositivo.Size = new System.Drawing.Size(319, 21);
+            this.cbbDispositivo.TabIndex = 5;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.btnLimpar.Location = new System.Drawing.Point(413, 125);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(201, 61);
+            this.btnLimpar.TabIndex = 6;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.btnAbrir.Location = new System.Drawing.Point(413, 245);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(201, 61);
+            this.btnAbrir.TabIndex = 7;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // frmWebCam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 450);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.cbbDispositivo);
+            this.Controls.Add(this.lblDispositivo);
+            this.Controls.Add(this.btnCapturar);
+            this.Controls.Add(this.pctCapturarImagem);
+            this.Name = "frmWebCam";
+            this.Text = "WebCam";
+            this.Load += new System.EventHandler(this.frmWebCam_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctCapturarImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctCapturarImagem;
+        private System.Windows.Forms.Button btnCapturar;
+        private System.Windows.Forms.Label lblDispositivo;
+        private System.Windows.Forms.ComboBox cbbDispositivo;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.SaveFileDialog sfdSalvarImagem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnAbrir;
+    }
+}
