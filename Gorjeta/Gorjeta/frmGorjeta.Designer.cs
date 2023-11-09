bb83f12f5062084b55e714aa6d1c6716b30cdb88
DigoPires <rodrigo_pires2806@outlook.com>
2023-11-09 12:03:55 -0300

    gorjeta

---
+++ Gorjeta/Gorjeta/frmGorjeta.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace Gorjeta
+{
+    partial class frmGorjeta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblValor = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(168, 45);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(283, 29);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Calculadora de Gorjeta";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValor.Location = new System.Drawing.Point(35, 99);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(131, 20);
+            this.lblValor.TabIndex = 1;
+            this.lblValor.Text = "Valor da conta:";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.txtValor.Location = new System.Drawing.Point(39, 126);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(532, 26);
+            this.txtValor.TabIndex = 2;
+            this.txtValor.Tag = "";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(39, 197);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(532, 28);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // frmGorjeta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 529);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.lblValor);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "frmGorjeta";
+            this.Text = "Gorjeta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
